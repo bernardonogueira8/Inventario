5428caa20d947cc760acf2537ad663899863d733
Bernardo Nogueira da Silva <Samuraiflamesf@gmail.com>
2024-07-29 16:51:30 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,6 +79,26 @@ def criar_arquivo_zip(arquivos):
     return buffer_zip
 
 
+def gerar_planilha_conferencia(df, nome):
+    df_conferencia = df.copy()
+    df_conferencia["Contagem 1"] = df_conferencia["Contagem"]
+    df_conferencia["Contagem 2"] = df_conferencia["Contagem"]
+    df_conferencia["Contagem 3"] = df_conferencia["Contagem"]
+    df_conferencia["Valor Adotado"] = df_conferencia["Contagem"]
+    df_conferencia = df_conferencia[
+        [
+            "Medicamento",
+            "Lote",
+            "Data Vencimento",
+            "Contagem 1",
+            "Contagem 2",
+            "Contagem 3",
+            "Valor Adotado",
+        ]
+    ]
+    return estilizar_dataframe(df_conferencia, nome)
+
+
 def main():
     st.title("Sistema de Inventário")
 
@@ -104,9 +124,7 @@ def main():
 
                 # Gerando nome das planilhas
                 data_atual = datetime.now().strftime("%Y%m%d")
-                nome_arquivo_estoque = (
-                    f"DadosEstoque_{data_atual}_{item_selecionado}.xlsx"
-                )
+                nome_arquivo_estoque = f"Estoque_{item_selecionado}_{data_atual}.xlsx"
                 # Estilizando dataframes
                 wb_estoque = Workbook()
                 ws_estoque = wb_estoque.active
@@ -119,7 +137,7 @@ def main():
 
                 if "Contagem" not in estoque_df.columns:
                     estoque_df["Contagem"] = None
-                
+
                 estoque_df = estoque_df[
                     ["Medicamento", "Lote", "Data Vencimento", "Contagem"]
                 ]
@@ -149,22 +167,27 @@ def main():
 
                 merged_df = merged_df.sort_values(by="Medicamento")
 
-                nome_arquivo_1 = f"{item_selecionado}_{data_atual}_contagem1.xlsx"
-                nome_arquivo_2 = f"{item_selecionado}_{data_atual}_contagem2.xlsx"
-
+                nome_arquivo_1 = f"{item_selecionado}_contagem1_{data_atual}.xlsx"
+                nome_arquivo_2 = f"{item_selecionado}_contagem2_{data_atual}.xlsx"
+                nome_arquivo_conferencia = (
+                    f"{item_selecionado}_{data_atual}_conferencia.xlsx"
+                )
 
                 wb1 = estilizar_dataframe(merged_df, "Contagem 1")
                 wb2 = estilizar_dataframe(merged_df, "Contagem 2")
+                wb_conferencia = gerar_planilha_conferencia(merged_df, "Conferência")
 
                 # Convertendo para bytes
                 excel_bytes_1 = to_excel_bytes(wb1)
                 excel_bytes_2 = to_excel_bytes(wb2)
+                excel_bytes_conferencia = to_excel_bytes(wb_conferencia)
 
                 # Criando arquivo ZIP
                 arquivos = [
                     (nome_arquivo_estoque, excel_bytes_estoque),
                     (nome_arquivo_1, excel_bytes_1),
                     (nome_arquivo_2, excel_bytes_2),
+                    (nome_arquivo_conferencia, excel_bytes_conferencia),
                 ]
                 arquivo_zip_bytes = criar_arquivo_zip(arquivos)
 
@@ -176,7 +199,103 @@ def main():
                 st.download_button(
                     label="Baixar Todos os Arquivos",
                     data=arquivo_zip_bytes,
-                    file_name=f"{item_selecionado}_{data_atual}_contagens.zip",
+                    file_name=f"{item_selecionado}_{data_atual}_arquivos.zip",
+                    mime="application/zip",
+                )
+
+    if opcao == "Gerar apuração":
+        st.subheader("Gerar Apuração")
+        estoque_file2 = st.file_uploader("Upload da planilha de Estoque:", type=["xlsx"])
+        conferencia_file = st.file_uploader("Upload da planilha de Conferencia:", type=["xlsx"])
+
+        if estoque_file2 and conferencia_file:
+            conferencia_df = carregar_planilha(conferencia_file, skiprows=0)
+            conferencia_df = conferencia_df[
+                ["Medicamento", "Lote", "Data Vencimento", "Valor Adotado"]
+            ]
+            conferencia_df = (
+                conferencia_df.groupby(["Medicamento", "Lote", "Data Vencimento"])[
+                    "Valor Adotado"
+                ]
+                .sum()
+                .reset_index()
+            )
+            estoque_df = carregar_planilha(estoque_file2, skiprows=7)
+            estoque_df = estoque_df[
+                [
+                    "Código Simpas",
+                    "Medicamento",
+                    "Lote",
+                    "Data Vencimento",
+                    "Quantidade Encontrada",
+                    "Valor Unitário",
+                    "Programa Saúde",
+                ]
+            ]
+            estoque_df["Lote"] = estoque_df["Lote"].astype(str)
+            estoque_df["Código Simpas"] = estoque_df["Código Simpas"].astype(str)
+            estoque_df = (
+                estoque_df.groupby(
+                    [
+                        "Código Simpas",
+                        "Medicamento",
+                        "Lote",
+                        "Data Vencimento",
+                        "Valor Unitário",
+                        "Programa Saúde",
+                    ]
+                )["Quantidade Encontrada"]
+                .sum()
+                .reset_index()
+            )
+
+            df = pd.merge(
+                conferencia_df,
+                estoque_df,
+                how="left",
+                on=["Lote", "Medicamento", "Data Vencimento"],
+            )
+
+            df = df.rename(
+                columns={
+                    "Data Vencimento": "Validade",
+                    "Quantidade Encontrada": "SIGAF",
+                    "Valor Adotado": "Contagem",
+                }
+            )
+
+            df['Contagem'] = pd.to_numeric(df['Contagem'], errors='coerce')
+            df['SIGAF'] = pd.to_numeric(df['SIGAF'], errors='coerce')
+            df['Valor Unitário'] = pd.to_numeric(df['Valor Unitário'], errors='coerce')
+
+            df["Dirença"] = df["Contagem"] - df["SIGAF"]
+            df["Vlr Total"] = df["Contagem"] * df["Valor Unitário"]
+            df["Vlr Divergencia"] = df["Dirença"] * df["Valor Unitário"]
+
+            new = [
+                "Código Simpas",
+                "Medicamento",
+                "Lote",
+                "Validade",
+                "Contagem",
+                "SIGAF",
+                "Dirença",
+                "Valor Unitário",
+                "Vlr Total",
+                "Vlr Divergencia",
+                "Programa Saúde",
+            ]
+            df = df[new]
+
+            # Exibir tabelas resultantes
+            st.write("Resultado da Análise:")
+            st.dataframe(df)
+            
+            # Botão de download
+                st.download_button(
+                    label="Planilha de Conferencia",
+                    data=df,
+                    file_name=f"Conferencia_{data_atual}.xlsx",
                     mime="application/zip",
                 )
 
