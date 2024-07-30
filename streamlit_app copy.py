af336b7b2cbe55b1eeb8c27387b1d9bfcc4c0cd4
Bernardo Nogueira da Silva <Samuraiflamesf@gmail.com>
2024-07-30 12:19:15 -0300

    start

--- streamlit_app copy.py
+++ streamlit_app copy.py
@@ -8,37 +8,36 @@ from openpyxl.utils.dataframe import dataframe_to_rows
 import zipfile
 
 
-def carregar_planilha(file, skiprows):
+def carregar_planilha(file, skiprows=0):
+    """Carrega uma planilha Excel"""
     try:
-        planilha = pd.read_excel(file, skiprows=skiprows)
-        return planilha
+        return pd.read_excel(file, skiprows=skiprows)
     except Exception as e:
         st.error(f"Erro ao carregar o arquivo: {e}")
         return None
 
 
 def carregar_todas_abas(file):
+    """Carrega todas as abas de uma planilha Excel"""
     try:
         xls = pd.ExcelFile(file)
-        df = pd.concat(
+        return pd.concat(
             pd.read_excel(xls, sheet_name=sheet) for sheet in xls.sheet_names
         )
-        return df
     except Exception as e:
         st.error(f"Erro ao carregar o arquivo com múltiplas abas: {e}")
         return None
 
 
 def estilizar_dataframe(df, sheet_name):
+    """Estiliza um DataFrame e retorna um Workbook"""
     wb = Workbook()
     ws = wb.active
     ws.title = sheet_name
 
-    # Adicionar cabeçalho
     for r in dataframe_to_rows(df, index=False, header=True):
         ws.append(r)
 
-    # Estilo do cabeçalho
     header_font = Font(bold=True)
     alignment = Alignment(horizontal="center", vertical="center")
     thin_border = Border(
@@ -53,7 +52,6 @@ def estilizar_dataframe(df, sheet_name):
         cell.alignment = alignment
         cell.border = thin_border
 
-    # Estilo das células
     for row in ws.iter_rows(
         min_row=2, max_row=ws.max_row, min_col=1, max_col=ws.max_column
     ):
@@ -64,6 +62,7 @@ def estilizar_dataframe(df, sheet_name):
 
 
 def to_excel_bytes(wb):
+    """Converte um Workbook para bytes"""
     buffer = BytesIO()
     wb.save(buffer)
     buffer.seek(0)
@@ -71,6 +70,7 @@ def to_excel_bytes(wb):
 
 
 def criar_arquivo_zip(arquivos):
+    """Cria um arquivo ZIP a partir de uma lista de arquivos"""
     buffer_zip = BytesIO()
     with zipfile.ZipFile(buffer_zip, "w") as zipf:
         for nome_arquivo, dados in arquivos:
@@ -80,10 +80,10 @@ def criar_arquivo_zip(arquivos):
 
 
 def gerar_planilha_conferencia(df, nome):
+    """Gera a planilha de conferência a partir de um DataFrame"""
     df_conferencia = df.copy()
-    df_conferencia["Contagem 1"] = df_conferencia["Contagem"]
-    df_conferencia["Contagem 2"] = df_conferencia["Contagem"]
-    df_conferencia["Contagem 3"] = df_conferencia["Contagem"]
+    for i in range(1, 4):
+        df_conferencia[f"Contagem {i}"] = df_conferencia["Contagem"]
     df_conferencia["Valor Adotado"] = df_conferencia["Contagem"]
     df_conferencia = df_conferencia[
         [
@@ -111,7 +111,6 @@ def main():
         st.subheader("Gerar Lista de Contagem")
 
         item_selecionado = st.text_input("Nome da Lista:")
-
         estoque_file1 = st.file_uploader(
             "Upload da planilha de Estoque:", type=["xlsx"]
         )
@@ -121,11 +120,9 @@ def main():
 
         if estoque_file1 and enderecos_file:
             estoque_df = carregar_planilha(estoque_file1, skiprows=7)
-            estoque = estoque_df
             enderecos_df = carregar_todas_abas(enderecos_file)
 
             if estoque_df is not None and enderecos_df is not None:
-                # Gerando nome das planilhas
                 data_atual = datetime.now().strftime("%Y%m%d")
                 nome_arquivo_estoque = f"Estoque_{item_selecionado}_{data_atual}.xlsx"
 
@@ -144,14 +141,8 @@ def main():
                         "LOTE": "Lote",
                     }
                 )
-
                 enderecos_df["Endereço"].fillna(enderecos_df["05/26"], inplace=True)
                 enderecos_df["Lote"] = enderecos_df["Lote"].astype(str).str.rstrip()
-                estoque = estoque.drop(columns=["Contagem"])
-                enderecos = enderecos_df
-                enderecos = enderecos[
-                    ["Endereço", "DESCRIÇÃO", "Programa", "Lote", "VALIDADE"]
-                ]
                 enderecos_df = enderecos_df[["Endereço", "Programa", "Lote"]]
 
                 merged_df = pd.merge(estoque_df, enderecos_df, on="Lote", how="left")
@@ -175,8 +166,8 @@ def main():
 
                 wb1 = estilizar_dataframe(merged_df, "Contagem")
                 wb_conferencia = gerar_planilha_conferencia(merged_df, "Conferência")
-                wb_endereco = estilizar_dataframe(enderecos, "Endereço")
-                wb_estoque = estilizar_dataframe(estoque, "Estoque")
+                wb_endereco = estilizar_dataframe(enderecos_df, "Endereço")
+                wb_estoque = estilizar_dataframe(estoque_df, "Estoque")
 
                 excel_bytes_estoque = to_excel_bytes(wb_estoque)
                 excel_bytes = to_excel_bytes(wb1)
@@ -211,10 +202,7 @@ def main():
         )
 
         if estoque_file2 and conferencia_file:
-            conferencia_df = carregar_planilha(conferencia_file, skiprows=0)
-            conferencia_df = conferencia_df[
-                ["Medicamento", "Lote", "Data Vencimento", "Valor Adotado"]
-            ]
+            conferencia_df = carregar_planilha(conferencia_file)
             conferencia_df = (
                 conferencia_df.groupby(["Medicamento", "Lote", "Data Vencimento"])[
                     "Valor Adotado"
@@ -222,20 +210,7 @@ def main():
                 .sum()
                 .reset_index()
             )
-            estoque_df = carregar_planilha(estoque_file2, skiprows=0)
-            estoque_df = estoque_df[
-                [
-                    "Código Simpas",
-                    "Medicamento",
-                    "Lote",
-                    "Data Vencimento",
-                    "Quantidade Encontrada",
-                    "Valor Unitário",
-                    "Programa Saúde",
-                ]
-            ]
-            estoque_df["Lote"] = estoque_df["Lote"].astype(str)
-            estoque_df["Código Simpas"] = estoque_df["Código Simpas"].astype(str)
+            estoque_df = carregar_planilha(estoque_file2)
             estoque_df = (
                 estoque_df.groupby(
                     [
@@ -254,10 +229,9 @@ def main():
             df = pd.merge(
                 conferencia_df,
                 estoque_df,
-                how="left",
                 on=["Lote", "Medicamento", "Data Vencimento"],
+                how="left",
             )
-
             df = df.rename(
                 columns={
                     "Data Vencimento": "Validade",
@@ -265,39 +239,32 @@ def main():
                     "Valor Adotado": "Contagem",
                 }
             )
-
-            df["Contagem"] = pd.to_numeric(df["Contagem"], errors="coerce")
-            df["SIGAF"] = pd.to_numeric(df["SIGAF"], errors="coerce")
-            df["Valor Unitário"] = pd.to_numeric(df["Valor Unitário"], errors="coerce")
-
             df["Diferença"] = df["Contagem"] - df["SIGAF"]
             df["Vlr Total"] = df["Contagem"] * df["Valor Unitário"]
             df["Vlr Divergencia"] = df["Diferença"] * df["Valor Unitário"]
 
-            new = [
-                "Código Simpas",
-                "Medicamento",
-                "Lote",
-                "Validade",
-                "Contagem",
-                "SIGAF",
-                "Diferença",
-                "Valor Unitário",
-                "Vlr Total",
-                "Vlr Divergencia",
-                "Programa Saúde",
+            df = df[
+                [
+                    "Código Simpas",
+                    "Medicamento",
+                    "Lote",
+                    "Validade",
+                    "Contagem",
+                    "SIGAF",
+                    "Diferença",
+                    "Valor Unitário",
+                    "Vlr Total",
+                    "Vlr Divergencia",
+                    "Programa Saúde",
+                ]
             ]
-            df = df[new]
 
-            # Estilizar o DataFrame
             wb = estilizar_dataframe(df, "Apuração")
             excel_bytes = to_excel_bytes(wb)
 
-            # Exibir tabelas resultantes
             st.write("Resultado da Análise:")
             st.dataframe(df)
 
-            # Botão de download
             st.download_button(
                 label="Baixar Planilha de Apuração",
                 data=excel_bytes,
@@ -310,52 +277,27 @@ def main():
         estoque_file3 = st.file_uploader(
             "Upload da planilha de Estoque Final:", type=["xlsx"]
         )
-        if estoque_file3:
 
+        if estoque_file3:
             estoque_df = carregar_planilha(estoque_file3, skiprows=7)
-            estoque_df = estoque_df[
-                [
-                    "Código Simpas",
-                    "Medicamento",
-                    "Quantidade Encontrada",
-                    "Programa Saúde",
+            estoque_df = (
+                estoque_df.groupby(["Código Simpas", "Medicamento", "Programa Saúde"])[
+                    "Quantidade Encontrada"
                 ]
-            ]
-            estoque_df["Código Simpas"] = estoque_df["Código Simpas"].astype(str)
-            df = (
-                estoque_df.groupby(
-                    [
-                        "Código Simpas",
-                        "Medicamento",
-                        "Programa Saúde",
-                    ]
-                )["Quantidade Encontrada"]
                 .sum()
                 .reset_index()
             )
-            df = df.sort_values(by="Código Simpas")
-            df = df.rename(
-                columns={
-                    "Quantidade Encontrada": "Quantidade",
-                }
+            estoque_df = estoque_df.sort_values(by="Código Simpas")
+            estoque_df = estoque_df.rename(
+                columns={"Quantidade Encontrada": "Quantidade"}
             )
-            new = [
-                "Código Simpas",
-                "Medicamento",
-                'Quantidade',
-                "Programa Saúde"
-            ]
-            df = df[new]
 
-            # Estilizar o DataFrame
-            wb = estilizar_dataframe(df, "Apuração SIMPAS")
+            wb = estilizar_dataframe(estoque_df, "Apuração SIMPAS")
             excel_bytes = to_excel_bytes(wb)
 
-            # Exibir tabelas resultantes
             st.write("Resultado da Análise:")
-            st.dataframe(df)
+            st.dataframe(estoque_df)
 
-            # Botão de download
             st.download_button(
                 label="Baixar Planilha de Apuração SIMPAS",
                 data=excel_bytes,
