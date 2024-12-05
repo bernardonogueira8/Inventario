2ae8f3495e0b9ec0e0049e3136e2f860d6e9a425
Bernardo Nogueira <62897976+Samuraiflamesf@users.noreply.github.com>
2024-12-04 23:35:44 -0300

    up

--- streamlit_app.py
+++ streamlit_app.py
@@ -263,7 +263,7 @@ def main():
         item_selecionado = st.text_input("Nome da Lista:")
 
         estoque_file1 = st.file_uploader(
-            "Upload da planilha de Estoque:", type=["xlsx"]
+            "Upload da planilha de Estoque:", type=["xlx"]
         )
         enderecos_file = st.file_uploader(
             "Upload da planilha de Endereços:", type=["xlsx"]
